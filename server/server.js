789f3d3837e1debe0dd0c466b8ad9634e3abb785
Rahul Kumar Yadav <ry573870@gmail.com>
2025-04-03 06:15:53 +0530

    contact form backend api complete

--- server/server.js
+++ server/server.js
@@ -4,12 +4,14 @@ const app = express();
 const PORT = 5000;
 
 const authRoute = require('./router/auth-router');
+const contactRoute = require("./router/contact-router");
 const { connectDB } = require('./utils/db');
 const errorMiddleware = require("./middlewares/error-middleware");
 
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
+app.use("/api/form", contactRoute);
 app.use(errorMiddleware);
 
 
