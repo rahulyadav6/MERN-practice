f1ba9a7c42d7c46bdc0eb3bd5b658b9d2c078744
Rahul Kumar Yadav <ry573870@gmail.com>
2025-04-03 06:51:13 +0530

    React routing done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css'
-
+/* eslint-disable no-unused-vars */
+import { BrowserRouter, Route, Routes }  from "react-router-dom"
+import pages from "./pages/Index"
 function App() {
 
   return (
     <>
-      <h1>Hello world!!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<pages.Home/>} />
+          <Route path="/about" element={<pages.About/>} />
+          <Route path="contact" element={<pages.Contact/>} />
+          <Route path="service" element={<pages.Service/>} />
+          <Route path="register" element={<pages.Register/>} />
+          <Route path="login" element={<pages.Login/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ client/src/pages/Index.js
@@ -0,0 +1,9 @@
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Login from './Login';
+import Register from './Register';
+import Service from './Service';
+
+const pages =  { Home, About, Contact, Login, Register, Service };
+export default pages;
